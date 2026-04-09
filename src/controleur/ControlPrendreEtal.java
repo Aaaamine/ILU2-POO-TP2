683942037c2926c241b021829c53b38320c15bb2
Aaaamine <160137312+Aaaamine@users.noreply.github.com>
2026-04-09 16:29:24 +0200

    Update ControlPrendreEtal.java

--- src/controleur/ControlPrendreEtal.java
+++ src/controleur/ControlPrendreEtal.java
@@ -1,30 +1,28 @@
 package controleur;
 
 import villagegaulois.Village;
+import personnages.Gaulois;
 
 public class ControlPrendreEtal {
 	private Village village;
 	private ControlVerifierIdentite controlVerifierIdentite;
 
-	public ControlPrendreEtal(ControlVerifierIdentite controlVerifierIdentite,
-			Village village) {
+	public ControlPrendreEtal(ControlVerifierIdentite controlVerifierIdentite, Village village) {
 		this.controlVerifierIdentite = controlVerifierIdentite;
 		this.village = village;
 	}
 
+	public boolean verifierIdentite(String nomVendeur) {
+		return controlVerifierIdentite.verifierIdentite(nomVendeur);
+	}
+
 	public boolean resteEtals() {
-		//TODO a completer, attention le retour ne dit pas etre false :-)
-		return false;
+		return village.rechercherEtalVide();
 	}
 
 	public int prendreEtal(String nomVendeur, String produit, int nbProduit) {
-		//TODO a completer
-		int numeroEtal = -1;
-		return numeroEtal;
-	}
-
-	public boolean verifierIdentite(String nomVendeur) {
-		//TODO a completer, attention le retour ne dit pas etre false :-)
-		return false;
+		Gaulois gaulois = village.trouverHabitant(nomVendeur);
+		return village.installerVendeur(gaulois, produit, nbProduit);
 	}
 }
+
